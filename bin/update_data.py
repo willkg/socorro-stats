db724fbf391a16a97b75c512c28355cbc03ec36c
Will Kahn-Greene <willkg@mozilla.com>
2023-07-13 19:02:01 -0400

    Clean up update_data.py script, fix replacement

--- bin/update_data.py
+++ bin/update_data.py
@@ -6,19 +6,16 @@
 
 import datetime
 import json
+import logging
 
 import requests
 
-import logging
 
-# These two lines enable debugging at httplib level (requests->urllib3->http.client)
-# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
-# The only thing missing will be the response.body which is not logged.
-try:
-    import http.client as http_client
-except ImportError:
-    # Python 2
-    import httplib as http_client
+# These two lines enable debugging at httplib level
+# (requests->urllib3->http.client) You will see the REQUEST, including HEADERS
+# and DATA, and RESPONSE with HEADERS but without DATA. The only thing missing
+# will be the response.body which is not logged.
+import http.client as http_client
 http_client.HTTPConnection.debuglevel = 1
 
 # You must initialize logging, otherwise you'll not see debug output.
@@ -31,51 +28,59 @@ requests_log.propagate = True
 
 API_URL = "https://crash-stats.mozilla.org/api/SuperSearch/"
 
-TODAY = datetime.datetime.today().strftime("%Y-%m-%d")
-YESTERDAY = (
-    (datetime.datetime.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-)
+
+def fetch_data(start_date, end_date):
+    default_params = {
+        "date": [f">={start_date}", f"<{end_date}"],
+        "_results_number": "0",
+    }
+
+    headers = {
+        "User-Agent": "socorro-stats <https://github.com/willkg/socorro-stats>",
+    }
+
+    # Get a facet of products and counts for yesterday
+    resp = requests.get(
+        API_URL,
+        params=dict(
+            _facets="product",
+            **default_params,
+        ),
+        headers=headers,
+    )
+
+    resp.raise_for_status()
+    resp_data = resp.json()
+    print(resp_data)
+    return resp_data
 
 
-DEFAULT_PARAMS = {
-    "date": [f">={YESTERDAY}", f"<{TODAY}"],
-    "_results_number": "0",
-}
+today_dt = datetime.datetime.today()
+end_date = today_dt.strftime("%Y-%m-%d")
+start_date = (today_dt - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
 
-HEADERS = {
-    "User-Agent": "socorro-stats <https://github.com/willkg/socorro-stats>",
-}
-
-
-# Get a facet of products and counts for yesterday
-resp = requests.get(
-    API_URL,
-    params=dict(
-        _facets="product",
-        **DEFAULT_PARAMS
-    ),
-    headers=HEADERS,
-)
-
-resp.raise_for_status()
-resp_data = resp.json()
-print(resp_data)
+resp_data = fetch_data(start_date, end_date)
 
 # Generate record
 data_item = {
-    "date": YESTERDAY,
+    "date": start_date,
     "total": resp_data["total"],
 }
-for item in resp_data["facets"]["product"]:
-    data_item[item["term"]] = item["count"]
+data_item.update(
+    {
+        item["term"]: item["count"]
+        for item in resp_data["facets"]["product"]
+    }
+)
 
 with open("socorro_stats.json", "r") as fp:
     all_data = json.load(fp)
 
 # Replace an existing item with the date or append the new item
 for i in range(len(all_data)):
-    if all_data[i]["date"] == YESTERDAY:
+    if all_data[i]["date"] == start_date:
         all_data[i] = data_item
+        break
 else:
     all_data.append(data_item)
 
